cc1bb693e9e5cdb1c1c232bcacaf498663b0b07a
Fábio Vitor Coelho Silva <48036134+FVitor7@users.noreply.github.com>
2020-11-25 03:00:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = create_app()
 
 if __name__ == '__main__':
     port = float(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
